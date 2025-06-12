53e6c97d326218236b909ee8eab6f54779522162
Facundo <facundodelacruz01@gmail.com>
2025-06-12 19:55:08 -0300

    se crea el servidor funcionando en el puerto 4000 y se conecta a la base de datos

---
+++ src/index.js
@@ -0,0 +1,10 @@
+require('./db/config.db')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+app.listen(process.env.PORT || 4000, ()=> {
+    console.log('servidor prendido en el puerto: ', process.env.PORT || 4000)
+})
+
