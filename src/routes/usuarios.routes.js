a60ad552bf22317eec157a1872f3c8ab23f606f6
Alvaro Cordoba <offaim123@gmail.com>
2025-06-13 14:34:26 -0300

    se crea el servicio de registrarusuario con su respectivo controlador, y definiendo su ruta en el usuarios.routes, tambien se hizo sus respectivas validaciones con el check de express validator y se creo los modelos del carrito y favoritos

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,4 +1,14 @@
 const { Router } = require("express");
+const { registrarUsuario } = require("../controllers/usuarios.controllers");
 const router = Router();
+const { check } = require("express-validator")
+
+router.post("/registro",[
+    check("nombreUsuario", "Campo USUARIO esta vacio").notEmpty(),
+    check("emailUsuario", "Campo EMAIL vacio").notEmpty(),
+    check("telefono", "Campo TELEFONO vacio").notEmpty(),
+    check("contrasenia", "Campo CONTRASEÑA vacio").notEmpty(),
+    check("contrasenia", "ERROR. caracteres soportados solo entre 8 y 40").isLength({min:8},{max:40}),
+], registrarUsuario)
 
 module.exports = router;
---
+++ src/services/usuarios.services.js
@@ -0,0 +1,49 @@
+const UsuariosModel = require("../models/usuarios.model")
+const CarritosModel = require("../models/carritos.model")
+const FavoritosModel = require("../models/favoritos.model")
+const argon = require("argon2")
+const { validationResult } = require("express-validator")
+
+
+const registrarUsuarioBD = async (body,req) =>{
+    const errorValidator = validationResult(req)
+   
+
+    if(!errorValidator.isEmpty()){
+        return {
+            msg:errorValidator.array(),
+            statusCode: 422
+        }
+        
+    }
+    try {
+        const nuevoUsuario = new UsuariosModel(body)
+        nuevoUsuario.contrasenia = await argon.hash(body.contrasenia)
+        const nuevoCarrito = new CarritosModel({idUsuario: nuevoUsuario._id})
+        const nuevoFavoritos = new FavoritosModel({idUsuario: nuevoUsuario._id})
+
+        nuevoUsuario.idCarrito = nuevoCarrito._id
+        nuevoUsuario.idFavoritos = nuevoFavoritos._id
+
+        await nuevoCarrito.save()
+        await nuevoFavoritos.save()
+        await nuevoUsuario.save()
+
+        return{
+            msg: "Usuario registrado con exito!",
+            statusCode: 201
+        }
+    } catch (error) {
+        return{
+            error, 
+            statusCode:500
+        }
+    }
+
+}
+
+module.exports = {
+    registrarUsuarioBD,
+
+
+}
