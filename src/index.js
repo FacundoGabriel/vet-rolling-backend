76b5a3d3dbf33017668d38bff481b16b6db2d4ce
Gabriel <gabrielfernandez090997@gmail.com>
2025-06-13 02:19:53 -0300

    Se añadieron las librerias cors, morgan y la ruta /api

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-require('./db/config.db')
-const express = require('express')
-const app = express()
+require("./db/config.db");
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.listen(process.env.PORT || 4000, ()=> {
-    console.log('servidor prendido en el puerto: ', process.env.PORT || 4000)
-})
+app.use(cors());
+app.use(morgan("dev"));
 
+app.use("/api", require("./routes/index.routes"));
+
+app.listen(process.env.PORT || 4000, () => {
+  console.log("servidor prendido en el puerto: ", process.env.PORT || 4000);
+});
