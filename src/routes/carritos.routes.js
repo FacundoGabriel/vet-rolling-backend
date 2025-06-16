c04f25cc3d0e105a35e49bd211a06010f881f51d
Gabriel <gabrielfernandez090997@gmail.com>
2025-06-16 19:05:40 -0300

    Se agregaron las validaciones en las rutas del carrito

--- src/routes/carritos.routes.js
+++ src/routes/carritos.routes.js
@@ -4,12 +4,41 @@ const {
   agregarProductoCarrito,
   eliminarProductoCarrito,
 } = require("../controllers/carritos.controllers");
+const validarCampos = require("../middlewares/validarCampos");
+const { check } = require("express-validator");
+const auth = require("../middlewares/auth");
 const router = Router();
 
-router.get("/obtenerProductos", obtenerProductosDelCarrito);
+router.get(
+  "/obtenerProductos",
+  auth(["usuario", "admin"]),
+  obtenerProductosDelCarrito
+);
 
-router.put("/agregarProducto/:idProducto", agregarProductoCarrito);
+router.put(
+  "/agregarProducto/:idProducto",
+  [
+    check(
+      "id",
+      "ERROR. ID incorrecto. El formato no corresponde a mongoose"
+    ).isMongoId(),
+  ],
+  auth(["usuario", "admin"]),
+  validarCampos,
+  agregarProductoCarrito
+);
 
-router.put("/eliminarProducto/:idProducto", eliminarProductoCarrito);
+router.put(
+  "/eliminarProducto/:idProducto",
+  [
+    check(
+      "id",
+      "ERROR. ID incorrecto. El formato no corresponde a mongoose"
+    ).isMongoId(),
+  ],
+  auth(["usuario", "admin"]),
+  validarCampos,
+  eliminarProductoCarrito
+);
 
 module.exports = router;
