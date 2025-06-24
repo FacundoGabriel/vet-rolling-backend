448ecd26a79d03649dc821ef25ceb5e905566861
Alvaro Cordoba <offaim123@gmail.com>
2025-06-23 21:40:36 -0300

    se corrige unos checks en la ruta de planes

--- src/routes/planes.routes.js
+++ src/routes/planes.routes.js
@@ -18,14 +18,14 @@ router.put("/agregarImagen/:id",
 
 router.get("/", obtenerPlanes)
 router.get("/:idPlan",  [
+    check("idPlan", "ID incorrecto. Formato no corresponde a mongoose").isMongoId()
+], validarCampos, obtenerUnPlan)
+router.put("/editarPlan/:idPlan", [
     check("nombre", "Campo NOMBRE esta vacio").notEmpty(),
     check("descripcion", "Campo DESCRIPCION esta vacio").notEmpty(),
     check("precio", "Campo PRECIO esta vacio").notEmpty(),
     check("servicios", "Campo SERVICIOS vacio")
     .isArray({ min: 1 }),
-    check("idPlan", "ID incorrecto. Formato no corresponde a mongoose").isMongoId()
-], validarCampos, obtenerUnPlan)
-router.put("/editarPlan/:idPlan", [
     check("idPlan", "ID incorrecto. Formato no corresponde a mongoose").isMongoId()
 ], validarCampos,auth("admin"), editarPlan)
 router.delete("/eliminarPlan/:idPlan",  [
